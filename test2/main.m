243f74f60793e3605234248c12cce79a9b6fe9ef
Bonan Zhang <bonanzhang@Kais-iMac.local>
2014-09-11 14:15:00 +1000

    Initial Commit

---
+++ test2/main.m
@@ -0,0 +1,22 @@
+//
+//  main.m
+//  test2
+//
+//  Created by Bonan Zhang on 11/09/2014.
+//  Copyright (c) 2014 fireup. All rights reserved.
+//
+
+#import <Foundation/Foundation.h>
+
+int main(int argc, const char * argv[])
+{
+
+    @autoreleasepool {
+        
+        // insert code here...
+        NSLog(@"Hello, World!");
+        
+    }
+    return 0;
+}
+
